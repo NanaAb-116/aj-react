0031a49678b08bf102fe964ea494d5dca5bb0214
NanaAb-116 <nanaabboateng@gmail.com>
2022-10-23 23:24:55 +0000

    fix firebase

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,15 +1,15 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from '@firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
 const firebaseConfig = {
-  apiKey: 'AIzaSyCkb22Tc_tPHfooQ1T1Ps0xGTmTiOHgKeE',
-  authDomain: 'new-practice-project-fe14d.firebaseapp.com',
-  projectId: 'new-practice-project-fe14d',
-  storageBucket: 'new-practice-project-fe14d.appspot.com',
-  messagingSenderId: '258582592492',
-  appId: '1:258582592492:web:29c7a4ec6301103e939ec9',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// initialize service
+
 export const db = getFirestore(app);
