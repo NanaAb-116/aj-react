cf76cbac851d93bf49b13acca9d98f04a46123d4
NanaAb-116 <nanaabboateng@gmail.com>
2023-12-29 00:28:21 +0000

    fix year on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   FaFacebookF,
   FaSoundcloud,
@@ -6,55 +6,55 @@ import {
   FaInstagram,
   FaSpotify,
   FaYoutube,
-} from 'react-icons/fa';
+} from "react-icons/fa";
 
 function Footer() {
   return (
     <div>
-      <section className='footer'>
-        <div className='container'>
-          <div className='row'>
-            <div className='col text-center'>
-              <div className='footer_socials'>
+      <section className="footer">
+        <div className="container">
+          <div className="row">
+            <div className="col text-center">
+              <div className="footer_socials">
                 <a
-                  target='blank'
-                  href='https://soundcloud.com/nana-a-j/sets/train-of-thought'
+                  target="blank"
+                  href="https://soundcloud.com/nana-a-j/sets/train-of-thought"
                 >
-                  <FaSoundcloud className='mx-2' />
+                  <FaSoundcloud className="mx-2" />
                 </a>
                 <a
-                  target='blank'
-                  href='https://www.facebook.com/atotheflippingj/'
+                  target="blank"
+                  href="https://www.facebook.com/atotheflippingj/"
                 >
-                  <FaFacebookF className='mx-2' />
+                  <FaFacebookF className="mx-2" />
                 </a>
-                <a target='blank' href='https://www.instagram.com/nana_a.j/'>
-                  <FaInstagram className='mx-2' />
-                </a>
-                <a
-                  target='blank'
-                  href='https://twitter.com/atotheflippingj?s=20&t=jgDe3BM8MflcevUmnfSs5w'
-                >
-                  <FaTwitter className='mx-2' />
+                <a target="blank" href="https://www.instagram.com/nana_a.j/">
+                  <FaInstagram className="mx-2" />
                 </a>
                 <a
-                  target='blank'
-                  href='https://open.spotify.com/artist/1NKZ7szc37Mlo5wTwm2tHA?si=mohAOJDFQXqSiMkhDFVI7g'
+                  target="blank"
+                  href="https://twitter.com/atotheflippingj?s=20&t=jgDe3BM8MflcevUmnfSs5w"
                 >
-                  <FaSpotify className='mx-2' />
+                  <FaTwitter className="mx-2" />
                 </a>
                 <a
-                  target='blank'
-                  href='https://www.youtube.com/playlist?list=OLAK5uy_kKdgIi68Smtyjd8GrEc-jrad3q2kf5Q6s'
+                  target="blank"
+                  href="https://open.spotify.com/artist/1NKZ7szc37Mlo5wTwm2tHA?si=mohAOJDFQXqSiMkhDFVI7g"
                 >
-                  <FaYoutube className='mx-2' />
+                  <FaSpotify className="mx-2" />
+                </a>
+                <a
+                  target="blank"
+                  href="https://www.youtube.com/playlist?list=OLAK5uy_kKdgIi68Smtyjd8GrEc-jrad3q2kf5Q6s"
+                >
+                  <FaYoutube className="mx-2" />
                 </a>
               </div>
             </div>
           </div>
-          <div className='row'>
-            <div className='col text-center'>
-              <p>©Nana A.J 2022</p>
+          <div className="row">
+            <div className="col text-center">
+              <p>©Nana A.J {new Date().getFullYear()}</p>
             </div>
           </div>
         </div>
