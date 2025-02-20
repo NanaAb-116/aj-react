9e242ac330936ad40ad64ae1ca1c3bbf5d3942cc
NanaAb-116 <nanaabboateng@gmail.com>
2025-02-20 19:17:29 +0000

    update videos

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -4,20 +4,72 @@ function Videos() {
   return (
     <div>
       <section>
-        <div className="container">
+        <div className="container mb-2">
           <div className="row mx-auto">
             <div className="col-md-12">
-              <h2 className="text-center my-5">Never Left</h2>
+              <h2 className="text-center my-5">
+                Nana A.J - Risky (Performance Video)
+              </h2>
             </div>
             <div className="embed-responsive embed-responsive-16by9">
               <iframe
                 width="560"
                 height="315"
-                src="https://www.youtube.com/embed/2YE6mKpCYCI"
+                src="https://www.youtube.com/embed/cdOLag5R88g?si=k5Sg9OT0bQ_YNHnW"
                 title="YouTube video player"
                 frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerpolicy="strict-origin-when-cross-origin"
                 allowfullscreen
+              ></iframe>
+            </div>
+            <div className="col-md-12">
+              <h2 className="text-center my-5">
+                Nana A.J live at Privat Club Berlin (SPH Music Masters Vorrunde)
+              </h2>
+            </div>
+            <div className="embed-responsive embed-responsive-16by9">
+              <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/6q4KS2PpJMs?si=Xd9URuY4x0ArNnIE"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerpolicy="strict-origin-when-cross-origin"
+                allowfullscreen
+              ></iframe>
+            </div>
+            <div className="col-md-12">
+              <h2 className="text-center my-5">
+                Nana A.J - Live at U&D Festival Würzburg 2022
+              </h2>
+            </div>
+            <div className="embed-responsive embed-responsive-16by9">
+              <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/tk_Y5Plwz6A?si=H8HFiBYfKDIkhW-E"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerpolicy="strict-origin-when-cross-origin"
+                allowfullscreen
+              ></iframe>
+            </div>
+            <div className="col-md-12">
+              <h2 className="text-center my-5">NEVER QUIT</h2>
+            </div>
+            <div className="embed-responsive embed-responsive-16by9">
+              <iframe
+                className="embed-responsive-item pb-5"
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/J3HzFwOdr9M"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
               ></iframe>
             </div>
             <div className="col-md-12">
@@ -36,66 +88,17 @@ function Videos() {
               ></iframe>
             </div>
             <div className="col-md-12">
-              <h2 className="text-center my-5">ROUND AND ROUND LYRIC VIDEO</h2>
-            </div>
-            <div className="embed-responsive embed-responsive-16by9">
-              <iframe
-                className="embed-responsive-item pb-5"
-                width="560"
-                height="315"
-                src="https://www.youtube.com/embed/U_bXavZ8urk"
-                title="YouTube video player"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-              ></iframe>
-            </div>
-            <div className="col-md-12">
-              <h2 className="text-center my-5">NEVER QUIT</h2>
-            </div>
-            <div className="embed-responsive embed-responsive-16by9">
-              <iframe
-                className="embed-responsive-item pb-5"
-                width="560"
-                height="315"
-                src="https://www.youtube.com/embed/J3HzFwOdr9M"
-                title="YouTube video player"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-              ></iframe>
-            </div>
-
-            <div className="col-md-12">
-              <h2 className="text-center my-5">
-                PLAYING AT THE CLIMATE JUSTICE FESTIVAL 2021 IN BERLIN
-              </h2>
+              <h2 className="text-center my-5">Never Left</h2>
             </div>
             <div className="embed-responsive embed-responsive-16by9">
               <iframe
                 width="560"
                 height="315"
-                src="https://www.youtube.com/embed/Lle46-WvfSU"
+                src="https://www.youtube.com/embed/2YE6mKpCYCI"
                 title="YouTube video player"
-                frameBorder="0"
+                frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-              ></iframe>
-            </div>
-            <div className="col-md-12">
-              <h2 className="text-center my-5">
-                Nana A.J live at Benefit Concert
-              </h2>
-            </div>
-            <div className="embed-responsive embed-responsive-16by9">
-              <iframe
-                width="560"
-                height="315"
-                src="https://www.youtube.com/embed/UhjRu8wqEg4"
-                title="YouTube video player"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
+                allowfullscreen
               ></iframe>
             </div>
           </div>
